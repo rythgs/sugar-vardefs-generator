95f1a4910b0e9b6170a2d71718cfae167e6440cc
Ryota Higashi <me@higashiryota.com>
2020-08-03 21:40:27 +0900

    react-syntax-highlighter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { throttle } from 'lodash'
+import { throttle } from 'lodash-es'
 import React, { useState } from 'react'
 import styled from 'styled-components'
 
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,24 +1,30 @@
-// import '../highlight.css'
-
-// import Prism from 'prismjs'
-// import loadLanguages from 'prismjs/components'
 import PropTypes from 'prop-types'
 import React from 'react'
 import SyntaxHighlighter from 'react-syntax-highlighter'
+import { monokaiSublime } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
 import styled from 'styled-components'
 
 import Pane from './Pane'
 
-// loadLanguages(['php'])
+const Preview = ({ text }) => (
+  <Wrapper>
+    <SyntaxHighlighter
+      language="php"
+      style={{
+        ...monokaiSublime,
+        hljs: {
+          ...monokaiSublime.hljs,
+          background: 'transparent',
+          padding: '1rem',
+          margin: 0,
+        },
+      }}
+    >
+      {text}
+    </SyntaxHighlighter>
+  </Wrapper>
+)
 
-const Preview = ({ text }) => {
-  // useEffect(() => Prism.highlightAll())
-  return (
-    <Wrapper>
-      <SyntaxHighlighter language="php">{text}</SyntaxHighlighter>
-    </Wrapper>
-  )
-}
 Preview.propTypes = {
   text: PropTypes.string,
 }
