1c3ef6d21d1d3e1d11f3e512e4edf7db38866d51
Ryota Higashi <higashi@systemplanet.co.jp>
2020-08-06 18:21:58 +0900

    Help Drawer （作成中...）

---
+++ src/components/Help.tsx
@@ -0,0 +1,11 @@
+import { WarningOutlined } from '@ant-design/icons'
+import { Typography } from 'antd'
+import React, { FC } from 'react'
+
+const Help: FC = () => (
+  <Typography.Paragraph>
+    <WarningOutlined /> 作成中...
+  </Typography.Paragraph>
+)
+
+export default Help
--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,19 +1,44 @@
-import { GithubOutlined } from '@ant-design/icons'
-import { Menu } from 'antd'
 import React, { FC } from 'react'
+import styled from 'styled-components'
+
+type LinkInfo = {
+  name: string
+  url: string
+  content: React.ReactNode
+  onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
+}
 
 type Props = {
+  list: LinkInfo[]
   style?: React.CSSProperties
 }
 
-const SocialLinks: FC<Props> = ({ style }) => (
-  <Menu theme="light" mode="horizontal" selectable={false} style={style}>
-    <Menu.Item>
-      <a href="https://github.com/rythgs/sugar-vardefs-generator">
-        <GithubOutlined style={{ margin: '0 1rem' }} />
-      </a>
-    </Menu.Item>
-  </Menu>
+const Nav = styled.nav`
+  display: flex;
+  & > div {
+    padding: 0 1rem;
+    cursor: pointer;
+  }
+
+  a {
+    color: inherit;
+  }
+`
+
+const SocialLinks: FC<Props> = ({ list, style }) => (
+  <Nav style={style}>
+    {list.map(({ name, url, content, onClick }) => (
+      <div key={name}>
+        {onClick ? (
+          <div onClick={onClick} aria-hidden="true">
+            {content}
+          </div>
+        ) : (
+          <a href={url}>{content}</a>
+        )}
+      </div>
+    ))}
+  </Nav>
 )
 
 export default SocialLinks
